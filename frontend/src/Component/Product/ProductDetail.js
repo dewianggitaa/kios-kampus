ce03aff35d485a7cbe2baa582b14bbd9ef591afb
@danggita000 <danggita000@gmail.com>
2024-11-11 13:39:50 +0700

    perbaiki tombol chat seller

--- frontend/src/Component/Product/ProductDetail.js
+++ frontend/src/Component/Product/ProductDetail.js
@@ -43,6 +43,10 @@ const ProductDetail = () => {
     fetchProductData()
   }, [id])
 
+  const whatsappLink = seller
+        ? `https://wa.me/${seller.no_wa}?text=Halo%20${seller.name},%20saya%20tertarik%20dengan%20produk%20Anda%20${product.product_name}`
+        : '#';
+
   return (
     <div className="px-8 py-6 min-h-screen bg-gray-50">
       <Navbar />
@@ -107,7 +111,7 @@ const ProductDetail = () => {
 
                 {/* Tombol Chat Seller */}
                 <a
-                  href={`https://wa.me/${seller.phone}`}
+                  href={whatsappLink}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mt-4 inline-flex items-center justify-center px-6 py-3 text-white font-semibold bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
