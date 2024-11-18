4fb84f3b6f3f88de7fcf6e4d2aa6c99c90e98ecf
@danggita000 <danggita000@gmail.com>
2024-11-18 07:31:09 +0700

    benerin backend

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,49 +1,33 @@
-import express, {  request, response } from 'express';
-import cors from 'cors'; // Impor cors
+import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import passport from 'passport';
-import './strategies/local-strategy.mjs'
-import routes from './routes/index.mjs'
+import './strategies/local-strategy.mjs';
+import routes from './routes/index.mjs';
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(
-    {origin: '*'}
-));
-
-app.use(cookieParser("sangatrahasiacuy"))
+app.use(cors({ origin: '*' }));
+app.use(cookieParser("sangatrahasiacuy"));
 app.use(session({
     secret: "inilebihrahasia",
     resave: false,
     saveUninitialized: false,
-    cookie: {
-        secure: false,
-        httpOnly: true, maxAge: 6000
-    }
-}))
+    cookie: { secure: false, httpOnly: true, maxAge: 6000 }
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
-app.use('/images', express.static('./images'))
-
-const PORT = process.env.PORT || 5000;
-
-
-
+app.use('/images', express.static('./images'));
 
 app.get("/", (req, res) => {
-    
-    console.log("hello world")
-    return res.send("hola juga")
-})
-
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("hello world");
+    return res.send("hola juga");
 });
+
+export default (req, res) => {
+    app(req, res);
+};
